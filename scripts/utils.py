739a53c393a92479e37771f27fc2f8067c02dca0
ahenokD1 <henokdes1@gmail.com>
2022-08-25 14:33:12 +0300

    userEngagement notebook added and utils convert_bytes_to_megabytes methoded added

--- scripts/utils.py
+++ scripts/utils.py
@@ -10,5 +10,36 @@ def convert_bytes_to_megabytes(df, bytes_data):
     """
     megabyte = 1*10e+5
     df[bytes_data] = df[bytes_data] / megabyte
-    
+
     return df[bytes_data]
+
+def missing_values_table(df):
+
+    # Total missing values
+    mis_val = df.isnull().sum()
+    
+    # Percentage of missing values
+    mis_val_percent = 100 * df.isnull().sum() / len(df)
+    
+    # dtype of missing values
+    mis_val_dtype = df.dtypes
+    
+    # Make a table with the results
+    mis_val_table = pd.concat([mis_val, mis_val_percent, mis_val_dtype], axis=1)
+    
+    # Rename the columns
+    mis_val_table_ren_columns = mis_val_table.rename(
+    columns = {0 : 'Missing Values', 1 : '% of Total Values', 2: 'Dtype'})
+    
+    # Sort the table by percentage of missing descending
+    mis_val_table_ren_columns = mis_val_table_ren_columns[
+        mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
+    '% of Total Values', ascending=False).round(1)
+    
+    # Print some summary information
+    print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+        "There are " + str(mis_val_table_ren_columns.shape[0]) +
+            " columns that have missing values.")
+    
+    # Return the dataframe with missing information
+    return mis_val_table_ren_columns
