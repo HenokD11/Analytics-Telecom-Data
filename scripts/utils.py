edd0d4429939b2c6ca8db54bb27afd1074e8730a
ahenokD1 <henokdes1@gmail.com>
2022-08-25 13:13:44 +0300

    userEngagement notebook added and utils convert_bytes_to_megabytes methoded added

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,3 +1,14 @@
 import pandas as pd
 import numpy as np
 
+def convert_bytes_to_megabytes(df, bytes_data):
+    """
+        This function takes the dataframe and the column which has the bytes values
+        returns the megabytesof that value
+        df -- dataframe
+        bytes_data --  column with bytes values
+    """
+    megabyte = 1*10e+5
+    df[bytes_data] = df[bytes_data] / megabyte
+    
+    return df[bytes_data]
